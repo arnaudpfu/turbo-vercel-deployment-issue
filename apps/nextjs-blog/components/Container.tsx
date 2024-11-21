1ceb843e6191fc844cb949048eb735a3b0a312fe
arnaudpfu <arnaudpfundstein.pro@gmail.com>
2024-11-21 13:31:28 +0100

    use cn in Container comp in next

--- apps/nextjs-blog/components/Container.tsx
+++ apps/nextjs-blog/components/Container.tsx
@@ -1,8 +1,9 @@
+import { cn } from '@internalpackage/utils';
 import { Button } from '@internalpackage/components/ui';
 
 export function Container({ className, children }: { className?: string; children: React.ReactNode }) {
     return (
-        <div className={'px-6 lg:px-8 ' + className}>
+        <div className={cn(className, 'px-6 lg:px-8')}>
             <div className="mx-auto max-w-2xl lg:max-w-7xl">{children}</div>
             <Button>Click me</Button>
         </div>
