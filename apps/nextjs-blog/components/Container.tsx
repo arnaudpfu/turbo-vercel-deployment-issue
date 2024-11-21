79f2444232f826f865417f72048e94cc35342a16
arnaudpfu <arnaudpfundstein.pro@gmail.com>
2024-11-21 13:24:58 +0100

    remove cn in Container comp in next

--- apps/nextjs-blog/components/Container.tsx
+++ apps/nextjs-blog/components/Container.tsx
@@ -1,9 +1,8 @@
-import { cn } from '@internalpackage/utils';
 import { Button } from '@internalpackage/components/ui';
 
 export function Container({ className, children }: { className?: string; children: React.ReactNode }) {
     return (
-        <div className={cn(className, 'px-6 lg:px-8')}>
+        <div className={'px-6 lg:px-8 ' + className}>
             <div className="mx-auto max-w-2xl lg:max-w-7xl">{children}</div>
             <Button>Click me</Button>
         </div>
